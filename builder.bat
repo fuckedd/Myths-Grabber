9a6d469122eebb9d433614fc1f30c515690bab2e
Anonymous <110276914+fuckedd@users.noreply.github.com>
2023-11-19 23:03:28 +0000

    Create builder.bat

---
+++ builder.bat
@@ -0,0 +1,47 @@
+@echo off
+setlocal
+
+REM Define the print function
+:print
+echo %1
+goto :eof
+
+REM Example usage of the print function
+call :print "Hello, World!"
+call :print "Hello, World!"
+call :print "Hello, World!"
+call :print "Hello, World!"
+call :print "Hello, World!"
+call :print "Hello, World!"
+call :print "Hello, World!"
+call :print "Hello, World!"
+call :print "Hello, World!"
+call :print "Hello, World!"
+call :print "Hello, World!"
+call :print "Hello, World!"
+call :print "Hello, World!"
+call :print "Hello, World!"
+call :print "Hello, World!"
+call :print "Hello, World!"
+call :print "Hello, World!"
+call :print "Hello, World!"
+call :print "Hello, World!"
+call :print "Hello, World!"
+call :print "Hello, World!"
+call :print "Hello, World!"
+call :print "Hello, World!"
+call :print "Hello, World!"
+call :print "Hello, World!"
+call :print "Hello, World!"
+call :print "Hello, World!"
+call :print "Hello, World!"
+call :print "Hello, World!"
+call :print "Hello, World!"
+call :print "Hello, World!"
+call :print "Hello, World!"
+call :print "Hello, World!"
+call :print "Hello, World!"
+call :print "Hello, World!"
+call :print "Hello, World!"
+
+endlocal
